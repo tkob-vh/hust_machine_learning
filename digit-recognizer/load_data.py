08bec591475d77ffaec4cad1e2706d2c118cce25
yyx <yuxin.yan.cs@gmail.com>
2024-05-14 14:46:23 +0800

    add load_data script

---
+++ digit-recognizer/load_data.py
@@ -0,0 +1,11 @@
+import os
+
+
+def load_data(data_path):
+    """
+    The data files train.csv and test.csv contain gray-scale images of hand-drawn digits, from zero through nine.
+    Each image is 28 pixels in height and 28 pixels in width, for a total of 784 pixels in total.
+    The training data set, (train.csv), has 785 columns. The first column, called "label", is the digit that was drawn by the user. The rest of the columns contain the pixel-values of the associated image.
+    """
+    images = []
+    return images
